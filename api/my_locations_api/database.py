201c37eefac66b8be84c0da36713b18f5a57e611
Mark <super16mail@gmail.com>
2023-03-23 23:20:57 +0400

    Refactor to URL constructor for database connection

--- api/my_locations_api/database.py
+++ api/my_locations_api/database.py
@@ -1,4 +1,5 @@
 from sanic.config import Config
+from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
@@ -12,11 +13,14 @@ class DatabaseConnection:
     """Connection class to PostgreSQL database."""
 
     def __init__(self, config: Config) -> None:
-        self._url: str = "".join([
-            f"{config.DB_ENGINE}://",
-            f"{config.DB_USER}:{config.DB_PASSWORD}@",
-            f"{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
-        ])
+        self._url: URL = URL.create(
+            config.DB_ENGINE,
+            config.DB_USER,
+            config.DB_PASSWORD,
+            config.DB_HOST,
+            config.DB_PORT,
+            config.DB_NAME,
+        )
         self._connection: AsyncEngine = create_async_engine(self._url)
         self._session_factory: async_sessionmaker[AsyncSession] = \
             async_sessionmaker(
